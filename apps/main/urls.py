c21acbacce8c26f51b6dd088955ad8c3a9edbdbb
JakubDomon <jakubdomonn@gmail.com>
2023-03-02 19:48:41 +0100

    Auth app

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('save/contact/', views.contactFormPost, name='saveContact'),
     path('save/opinions/', views.opinionFormPost, name='saveOpinions'),
     path('get/opinions/', views.opinionsGet, name='getOpinions'),
-    path('auth/login', views.login, name='login'),
 ]
