85bef8d1e9f104282a456c84cc38954c1346a802
JakubDomon <jakubdomonn@gmail.com>
2023-03-22 09:31:18 +0100

    Client chat panel and admin chat panel

--- apps/panel/urls.py
+++ apps/panel/urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Products urls
     path('panel/products/', views.ProductViewList.as_view(), name='panelOrders'),
-    path('panel/chat/', views.ChatLobbyViewList.as_view(), name='chat_lobby')
+    # Chat urls
+    path('panel/chat/', views.ChatLobbyViewList.as_view(), name='chat_lobby'),
+    path('panel/chat/<str:room>', views.ChatRoomViewList.as_view(), name='chat_room'),
+    path('panel/chat/admin/', views.ChatPanelAdminList.as_view(), name='admin_chat_panel'),
 ]
